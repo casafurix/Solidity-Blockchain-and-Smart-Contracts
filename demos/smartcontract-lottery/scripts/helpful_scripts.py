4a9075e71139ca8fe2aa1113ef10f1e26bf83723
casafurix <agnibha10@gmail.com>
2022-03-04 01:22:06 +0530

    Commit 11

--- demos/smartcontract-lottery/scripts/helpful_scripts.py
+++ demos/smartcontract-lottery/scripts/helpful_scripts.py
@@ -26,8 +26,9 @@ def get_account(index=None, id=None):
         or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
     ):
         return accounts[0]
-
-    return accounts.add(config["wallets"]["from_key"])
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
 
 
 contract_to_mock = {
