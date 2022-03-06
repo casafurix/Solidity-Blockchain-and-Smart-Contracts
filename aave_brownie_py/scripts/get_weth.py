c341de4187218608653936249fef3f368a880de1
casafurix <agnibha10@gmail.com>
2022-03-06 13:26:13 +0530

    Commit 13

--- aave_brownie_py/scripts/get_weth.py
+++ aave_brownie_py/scripts/get_weth.py
@@ -1,4 +1,8 @@
 # WETH -> ERC20 version of Ethereum, Wrapped ETH.
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+
+
 def main():
     get_weth()
 
@@ -9,4 +13,9 @@ def get_weth():
     """
     # ABI
     # address
-    pass
+    account = get_account()
+    weth = interface.IWETH(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    tx.wait(1)
+    print("Received 0.1 WETH")
+    return tx
--- demos/smartcontract-lottery/scripts/helpful_scripts.py
+++ demos/smartcontract-lottery/scripts/helpful_scripts.py
@@ -19,16 +19,14 @@ def get_account(index=None, id=None):
     # accounts.load("id")
     if index:
         return accounts[index]
-    if id:
-        return accounts.load(id)
-    if (
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
-    ):
-        return accounts[0]
-    if network.show_active() in config["networks"]:
+        if id:
+            return accounts.load(id)
+        if (
+            network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+            or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+        ):
+            return accounts[0]
         return accounts.add(config["wallets"]["from_key"])
-    return None
 
 
 contract_to_mock = {
