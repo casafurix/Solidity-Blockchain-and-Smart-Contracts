1ce6b83f2fb4c90e2cbde19fbf8c4c402c44c482
casafurix <agnibha10@gmail.com>
2022-03-12 21:05:22 +0530

    Commit 16 (error in deploying adv. collectible on rinkeby)

--- demos/nft-demo/contracts/AdvancedCollectible.sol
+++ demos/nft-demo/contracts/AdvancedCollectible.sol
@@ -35,10 +35,7 @@ contract AdvancedCollectible is ERC721, VRFConsumerBase {
         fee = _fee;
     }
 
-    function createCollectible(string memory tokenURI)
-        public
-        returns (bytes32)
-    {
+    function createCollectible() public returns (bytes32) {
         bytes32 requestId = requestRandomness(keyhash, fee);
         requestIdToSender[requestId] = msg.sender;
         emit requestedCollectible(requestId, msg.sender);
--- demos/nft-demo/scripts/advanced_collectible/deploy_and_create.py
+++ demos/nft-demo/scripts/advanced_collectible/deploy_and_create.py
@@ -1,21 +1,28 @@
 from random import sample
-from scripts.helpful_scripts import get_account
-from brownie import SimpleCollectible
-
-sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
-OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+from scripts.helpful_scripts import (
+    get_account,
+    OPENSEA_URL,
+    get_contract,
+    fund_with_link,
+)
+from brownie import AdvancedCollectible, network, config
 
 
 def deploy_and_create():
     account = get_account()
-    simple_collectible = SimpleCollectible.deploy({"from": account})
-    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
-    tx.wait(1)
-    print(
-        f"Awesome! We can now view this NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter()-1)}"
+    # We want to be able to use the deployed contracts if we are on a testnet.
+    # Otherwise, we want to deploy some mocks and use those on Rinkeby
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
     )
-    print("Please wait up to 20 minutes, and hit the refresh metadata button.")
-    return simple_collectible
+    fund_with_link(advanced_collectible.address)
+    creating_tx = advanced_collectible.createCollectible({"from": account})
+    creating_tx.wait(1)
+    print("New token has been created!")
 
 
 def main():
